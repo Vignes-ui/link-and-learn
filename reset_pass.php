90685de651bd7657d97db112aab0ed24e6771160
Chendur Manogaran <chendurmanogaranbellu@gmail.com>
2026-04-19 12:02:22 +0530

    Cleanup test scripts

--- reset_pass.php
+++
@@ -1,5 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;dbname=linklearn', 'root', '');
-$pass = password_hash('Password123!', PASSWORD_DEFAULT);
-$pdo->prepare('UPDATE users SET password_hash=? WHERE id=2')->execute([$pass]);
-echo "Password reset for user 2\n";
