e2c87472a0e065e79b1b15d923c83e2ee3bc6e5b
Chendur Manogaran <chendurmanogaranbellu@gmail.com>
2026-04-20 18:21:15 +0530

    Final clean v1 link-and-learn

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'free-hosting-package']),
+  globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
